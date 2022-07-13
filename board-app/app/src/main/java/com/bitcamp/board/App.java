5a8d5dfc9a4888669eccf1e8fd4f42bc06e04c04
Reese-Yoon <hyun5no@naver.com>
2022-07-13 20:37:12 +0900

    0713

--- board-app/app/src/main/java/com/bitcamp/board/App.java
+++ board-app/app/src/main/java/com/bitcamp/board/App.java
@@ -1,73 +1,116 @@
-
+/*
+ * 강사님 수업한 내용을 바탕으로 
+ * 게시판프로그램을 나름 나의방식으로 새로 만들어봄.
+ * 수업내용 : if else
+ * 복습 : switch구문 이용
+ * 
+ * 숙제 : class로 나눠서 해보기
+ */
 package com.bitcamp.board;
 
 public class App {
 
   public static void main(String[] args) {
 
-    java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
+    java.util.Scanner Input = new java.util.Scanner(System.in);
 
-      System.out.println("[ 게시판 어플리케이션 ]");
+    final int SIZE = 4; //게시글의 갯수
+    int index =0; //스택 화살표? 개발자가 봤을때 배열인덱스번호
+    int cho; //메뉴선택지
+    int no; //게시판 이용자가 봤을때 게시판번호(배열인덱스번호 +1 차이가 남.)
+
+    String[] title = new String[SIZE];      //게시글 제목
+    String[] content = new String[SIZE];    //게시글 내용
+    String[] password = new String[SIZE];   //게시글 비밀번호
+    String[] createdDate = new String[SIZE];//게시글 작성날짜
+    String[] writer = new String[SIZE];     //게시글 작성자
+    int[] viewCount = new int[SIZE];        //게시글 조회수
+
+
+    System.out.println("[ 게시판 게시물 ]");
+    System.out.println("환영합니다.^^");
+
+    while (true){ //무한 반복
+      System.out.println("-----------------------------");
+      System.out.println("메뉴 : ");
+      System.out.println("  1. 게시물 목록");
+      System.out.println("  2. 게시글 상세보기");
+      System.out.println("  3. 게시글 등록");
+      System.out.print("메뉴를 선택하세요.[1..3] (0:종료)");
+      cho = Input.nextInt();
       System.out.println();
-      System.out.println("환영합니다. ");
-      System.out.println();
+      System.out.println("-----------------------------");
 
-      while(true){
+      if(cho == 0){
+        System.out.println("~~~~~~~~END~~~~~~~~~~~");
+        System.out.println("안녕히 가세요~:-)");
+        break;
+      } //0을 입력받았을때 종료함.
 
-        System.out.println("메뉴 : ");
-        System.out.println("1 : 게시글 목록");
-        System.out.println("2 : 게시글 상세보기");
-        System.out.println();
-        java.lang.System.out.print("메뉴를 선택하세요.[1..2](0:exit)");
+      switch (cho){
+        case 1: 
+          System.out.println("[ 1. 게시글 목록 ]");
+          System.out.println("번호\t제목\t조회수\t작성자\t등록일\t");
+          for(int i=0; i<SIZE; i++){
+            System.out.printf("%d\t%s\t%d\t%s\t%s \n", 
+                i+1, title[i], viewCount[i],writer[i],createdDate[i]);
+          }
+          break;
 
-        int menuNo = keyboardInput.nextInt();
-        keyboardInput.nextLine(); //입력한 숫자 중 
+        case 2:
+          if(index==0){ //게시판 전체가 empty 일때
+            System.out.println("게시판이 비어져있습니다. 등록을 먼저 해주세요.");
+            break;
+          }
 
-        //System.out.println("==>"+ menuNo);
+          System.out.println("[ 2. 게시글 상세보기 ]");
+          System.out.print("조회할 게시글 번호를 입력해주세요. :");
+          no = Input.nextInt();
+          no -= 1; //배열번호와 게시글번호의 갭 해결 index = no
 
-        if(menuNo==0){
-          System.out.println("안녕히 가세요");
-          break; //반복문 탈출
-        } else if(menuNo==1){
-          System.out.println("[ 게시글 목록 ]");
-          System.out.println("번호 제목 조회수 작성자 등록일");
-          
-          System.out.print(1);
-          System.out.print("\t");
-          System.out.print("제목입니다.");
-          System.out.print('\t');
-          System.out.print(20 +"\t"); 
-          System.out.print("홍길동\t");
-          System.out.print("2022-07-08 \n");
-  
-          
-          System.out.print(2 + "\t" + "제목입니다.2" + '\t'+11 
-            +"\t"+"홍길동\t"+"2022-07-08 \n");
-          
-          System.out.println(3 + "\t제목입니다.3" + '\t'+31 
-          +"\t"+"임꺽정\t"+"2022-07-08");
-  
-          System.out.printf("%d\t%s\t%d\t%s\t%s\n",
-            4,"제목입니다4",45,"유관순","2022-07-08");
+          if(title[no]==null){ //자세히 보고 싶은 게시글이 비어져 있을때
+            System.out.printf("%d번째 게시글은 비어져 있습니다. \n",no);
+            break;
+          }
+
+          System.out.println("-------------------------------------");
+          System.out.printf("게시글 번호 : %d \n", no+1);
+          System.out.printf("제목 : %s \n",title[no]);
+          System.out.printf("내용 : %s \n",content[no]);
+          System.out.printf("조회수 : %d \n", viewCount[no]); //수정필요
+          System.out.printf("작성자 : %s \n",writer[no]);
+          break;
+
+        case 3:
+          System.out.println("[ 1. 게시글 등록 ]");
+          System.out.println("제목 : ");
+          title[index]=Input.next();
+          System.out.println("내용 : ");
+          content[index]=Input.next();
+          System.out.println("작성자 : ");
+          writer[index] = Input.next();
+          System.out.println("암호 : ");
+          password[index] = Input.next();
+
+          System.out.printf(" \"num[%d] - %d번째 게시글로 저장되었습니다.\" \n", index+1, index+1);
           System.out.println();
-        } else if(menuNo==2){
-          System.out.println(" 게시글 상세보기 ");
-          System.out.println("[ 보드 게시판 상세보기 ]");
 
-          System.out.printf("번호 : %d \n",1);
-          System.out.printf("제목 : %s \n","제목입니다.");
-          //String an = scan.nextLine();
-          System.out.printf("내용 : %s \n","내용입니다." );
-          System.out.printf("조회수 : %d \n",100);
-          System.out.printf("작성자 : %s \n","홍길동");
-          System.out.printf("등록일 : %s \n", "2022-07-08");
-          System.out.println();
+          System.out.println("-----개발자 메모--------------------");
+          System.out.printf("Current Array index : %d \n", index);
 
-        }else {
-          System.out.println("메뉴 번호가 옳지 않습니다!");
-          System.out.println();
-        }
-      } //while 반복문
-  } //main()
-}
- //keyboardInput.close();
+        if(index == SIZE){
+          System.out.println("게시판이 꽉찼습니다. 게시글을 등록할 수 없습니다.");
+          break; //게시판 Full
+          } else { index ++; } //새로운 게시글 준비
+
+          break;
+
+        default :
+          System.out.println("잘못입력하셨습니다. 다시 입력해주세요");
+          break;
+
+      } //switch 반복문
+    } //while 무한 반복
+
+  }
+} 
---
+++ classPra/app/src/main/java/com/study/pra/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+  
+  //필드
+  String model;
+  String company;
+  String color;
+  int maxSpeed;
+  int speed;
+  
+}
